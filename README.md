3f118ccd848603bbe5c762c1faaabe3a1c1c5958
serg <serg@itlbs.ru>
2017-03-06 20:02:58 +0300

    fix README, MANIFEST

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Parsers for pygster https://pypi.python.org/pypi/pygster
 Usage:
 
 `pygster [options] pygster_logparsers.PARSER logfile`
+
+PARSER is only HAProxyPygster on this moment...
+
+pygster options like as `logster` https://github.com/etsy/logster
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 
 __title__ = 'pygster_logparsers'
-__version__ = '0.1'
+__version__ = '0.1.1'
 __author__ = 'Serg Belyakov  <serg@itlbs.ru>'
 __copyright__ = 'Copyright 2014 OnBeep, Inc. and Contributors'
 __license__ = 'Apache License, Version 2.0'
@@ -13,11 +13,12 @@ setup(
   name = __title__,
   packages = [__title__, ],
   version = __version__,
-  description = 'Parsers for pygster https://pypi.python.org/pypi/pygster',
+  description = open('README.md').read(),
   author = 'Serg Belyakov',
   author_email = 'serg@itlbs.ru',
   url = 'https://github.com/serg-it/pygster-parsers',
   license=open('LICENSE').read(),
   install_requires=['pygster'],
+  package_dir={'pygster_logparsers': 'pygster_logparsers'},
   zip_safe=False,
 )
